15bd5e8b1aeb8cfa2aa928218c3da86dcc95e88d
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-08-30 12:11:02 +0000

    Remove configuration.rb from bootstrap module

--- lib/phlex/icons/bootstrap.rb
+++ lib/phlex/icons/bootstrap.rb
@@ -6,7 +6,7 @@ module Phlex
       require_relative 'bootstrap/base'
 
       Dir[File.join(__dir__, 'bootstrap', '*.rb')].each do |file|
-        next if ['base.rb', 'configuration.rb'].include?(::File.basename(file))
+        next if ['base.rb'].include?(::File.basename(file))
 
         require_relative file
       end
