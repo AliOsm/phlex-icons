9a4ad3ed9a5481f14bdf29d23851bd761ccce93c
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-09-01 23:24:34 +0300

    Add GitHub Actions workflow to publish lucide icons specific gem

---
+++ .github/scripts/prepare-lucide.sh
@@ -0,0 +1,13 @@
+packs_to_remove=("bootstrap" "flag" "hero" "radix" "remix")
+
+for pack in "${packs_to_remove[@]}"; do
+  rm -rf "lib/phlex/icons/$pack"
+  rm "lib/phlex/icons/$pack.rb"
+  sed -i "/require_relative 'phlex\/icons\/$pack'/d" lib/phlex_icons.rb
+done
+
+mv lib/phlex_icons.rb lib/phlex_icons_lucide.rb
+
+sed -i "s/spec.name = 'phlex-icons'/spec.name = 'phlex-icons-lucide'/" phlex-icons.gemspec
+
+mv phlex-icons.gemspec phlex-icons-lucide.gemspec
