96e2d3bd6ff33d4539b312c34f4a5f08ea25f885
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-09-01 11:19:04 +0000

    Create Phlex::Icons::Remix module

---
+++ lib/phlex/icons/remix.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Phlex
+  module Icons
+    module Remix
+      require_relative 'remix/base'
+
+      Dir[File.join(__dir__, 'remix', '*.rb')].each do |file|
+        next if ['base.rb'].include?(::File.basename(file))
+
+        require_relative file
+      end
+    end
+  end
+end
