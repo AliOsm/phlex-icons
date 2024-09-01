25d86be039b55c205ec88a72dfd25442e14378a0
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-09-01 11:39:34 +0000

    Lazy load radix and remix icon packs

--- lib/phlex/icons/radix.rb
+++ lib/phlex/icons/radix.rb
@@ -5,10 +5,12 @@ module Phlex
     module Radix
       require_relative 'radix/base'
 
+      extend Phlex::Icons::Helper
+
       Dir[File.join(__dir__, 'radix', '*.rb')].each do |file|
         next if ['base.rb'].include?(::File.basename(file))
 
-        require_relative file
+        autoload class_name_from_file_path(file), file
       end
     end
   end
--- lib/phlex/icons/remix.rb
+++ lib/phlex/icons/remix.rb
@@ -5,10 +5,12 @@ module Phlex
     module Remix
       require_relative 'remix/base'
 
+      extend Phlex::Icons::Helper
+
       Dir[File.join(__dir__, 'remix', '*.rb')].each do |file|
         next if ['base.rb'].include?(::File.basename(file))
 
-        require_relative file
+        autoload class_name_from_file_path(file), file
       end
     end
   end
