d49c892c07c324afe02932b81e88484807c7b8d3
Carl Mercier <carl@carlmercier.com>
2025-05-04 02:43:03 -0500

    rubocop

--- spec/phlex_icons/icon_spec.rb
+++ spec/phlex_icons/icon_spec.rb
@@ -8,13 +8,14 @@ require 'phlex-icons/icon'
 RSpec.describe PhlexIcons::Icon do
   describe '.call' do
     it 'calls the proper icon class' do
-      expect(PhlexIcons::Lucide::ArrowLeft).to receive(:new).with(class: 'xyz').and_return(PhlexIcons::Lucide::ArrowLeft.new)
+      expect(PhlexIcons::Lucide::ArrowLeft).to receive(:new).with(class: 'xyz')
+                                                            .and_return(PhlexIcons::Lucide::ArrowLeft.new)
       described_class.call('lucide/arrow-left', class: 'xyz')
     end
 
     it 'parses and overrides variant from name' do
-      expect(PhlexIcons::Lucide::ArrowLeft).to receive(:new).with(class: 'xyz',
-                                                                  variant: :solid).and_return(PhlexIcons::Lucide::ArrowLeft.new)
+      expect(PhlexIcons::Lucide::ArrowLeft).to receive(:new).with(class: 'xyz', variant: :solid)
+                                                            .and_return(PhlexIcons::Lucide::ArrowLeft.new)
       described_class.call('lucide/arrow-left:solid', class: 'xyz', variant: :outline)
     end
   end
