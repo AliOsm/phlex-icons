c0ed40e07174dffba077afff3f2c13aac192a310
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-05-09 17:06:38 +0300

    Remove the Rails helper as it conflicts with Rails constant (#31)

--- lib/phlex-icons/railtie.rb
+++ lib/phlex-icons/railtie.rb
@@ -1,22 +1,15 @@
 # frozen_string_literal: true
 
-require_relative 'rails/helpers'
-require_relative 'configuration' # Ensure configuration is loaded
+require_relative 'configuration'
 
 module PhlexIcons
   class Railtie < ::Rails::Railtie
     initializer 'phlex_icons.view_helpers' do
       ActiveSupport.on_load(:action_view) do
-        # Include the module containing the internal helper implementations
-        include PhlexIcons::Rails::Helpers
-
-        # Get the configured helper name
         helper_method_name = PhlexIcons.configuration.helper_method_name
 
-        # Define the public helper method dynamically
         define_method helper_method_name do |name, **options|
-          # Call the internal implementation
-          _render_phlex_icon(name, **options)
+          PhlexIcons::Icon.call(name, **options)&.html_safe
         end
       end
     end
