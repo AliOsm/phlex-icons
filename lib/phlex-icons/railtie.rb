c766577bf7a20f84e42ffd857e2586031db9768a
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-10-15 23:30:06 +0300

    Fix phlex_icon helper method conflict with Phlex::Kit components

--- lib/phlex-icons/railtie.rb
+++ lib/phlex-icons/railtie.rb
@@ -9,7 +9,15 @@ module PhlexIcons
         helper_method_name = PhlexIcons.configuration.helper_method_name
 
         define_method helper_method_name do |name, **options|
-          PhlexIcons::Icon.call(name, **options)&.html_safe
+          component, fiber_id = Thread.current[:__phlex_component__]
+
+          if component && fiber_id == Fiber.current.object_id
+            component.instance_exec { render PhlexIcons::Icon.new(name, **options) }
+
+            nil
+          else
+            PhlexIcons::Icon.call(name, **options)&.html_safe
+          end
         end
       end
     end
