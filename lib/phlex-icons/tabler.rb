43d5eeb79cec8b3c03d09e8cfe832daad991f386
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2026-01-02 01:12:29 +0000

    Re-generate Tabler icon pack

--- lib/phlex-icons/tabler.rb
+++ lib/phlex-icons/tabler.rb
@@ -2,7 +2,7 @@
 
 module PhlexIcons
   module Tabler # rubocop:disable Metrics/ModuleLength
-    VERSION = '3.36.0'
+    VERSION = '3.36.1'
     VARIANTS = %i[outline filled].freeze
 
     extend Phlex::Kit
