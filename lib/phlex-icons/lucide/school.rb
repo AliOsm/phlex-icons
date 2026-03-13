e6ac8a142d5a8551394a8feec198e8dcc4bc732a
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2026-03-13 00:35:38 +0000

    Re-generate Lucide icon pack

--- lib/phlex-icons/lucide/school.rb
+++ lib/phlex-icons/lucide/school.rb
@@ -15,13 +15,13 @@ module PhlexIcons
           stroke_linejoin: 'round'
         ) do |s|
           s.path(d: 'M14 21v-3a2 2 0 0 0-4 0v3')
-          s.path(d: 'M18 5v16')
+          s.path(d: 'M18 4.933V21')
           s.path(d: 'm4 6 7.106-3.79a2 2 0 0 1 1.788 0L20 6')
           s.path(
             d:
               'm6 11-3.52 2.147a1 1 0 0 0-.48.854V19a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-5a1 1 0 0 0-.48-.853L18 11'
           )
-          s.path(d: 'M6 5v16')
+          s.path(d: 'M6 4.933V21')
           s.circle(cx: '12', cy: '9', r: '2')
         end
       end
