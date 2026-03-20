9c4990c127d0a65dcd4c63b50e15bf0e84f054fa
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2026-03-20 00:34:29 +0000

    Re-generate Lucide icon pack

--- lib/phlex-icons/lucide/gpu.rb
+++ lib/phlex-icons/lucide/gpu.rb
@@ -14,8 +14,8 @@ module PhlexIcons
           stroke_linecap: 'round',
           stroke_linejoin: 'round'
         ) do |s|
+          s.path(d: 'M2 17h18a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H2')
           s.path(d: 'M2 21V3')
-          s.path(d: 'M2 5h18a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2.26')
           s.path(d: 'M7 17v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-3')
           s.circle(cx: '16', cy: '11', r: '2')
           s.circle(cx: '8', cy: '11', r: '2')
