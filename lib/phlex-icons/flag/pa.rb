5deb234f471e0021d98e0149d3a5a6df16b37054
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2026-04-10 00:20:58 +0000

    Re-generate Flag icon pack

--- lib/phlex-icons/flag/pa.rb
+++ lib/phlex-icons/flag/pa.rb
@@ -32,26 +32,21 @@ module PhlexIcons
         svg(
           **attrs, xmlns: 'http://www.w3.org/2000/svg', viewbox: '0 0 640 480'
         ) do |s|
-          s.defs do
-            s.clipPath(id: 'pa-a') { s.path(fill_opacity: '.7', d: 'M0 0h640v480H0z') }
-          end
-          s.g(clip_path: 'url(#pa-a)') do
-            s.path(fill: '#fff', d: 'M0 0h640v480H0z')
-            s.path(fill: '#fff', fill_rule: 'evenodd', d: 'M92.5 0h477.2v480H92.4z')
-            s.path(fill: '#db0000', fill_rule: 'evenodd', d: 'M323 3.6h358v221.7H323z')
-            s.path(
-              fill: '#0000ab',
-              fill_rule: 'evenodd',
-              d:
-                'M3.2 225.3h319.9V480H3.2zm211.6-47.6-42-29.4-41.7 29.6 15.5-48L105 100l51.6-.4 16-48 16.3 47.9h51.6l-41.5 30 15.9 48z'
-            )
-            s.path(
-              fill: '#d80000',
-              fill_rule: 'evenodd',
-              d:
-                'm516.9 413.9-42.4-27.7-42.1 28 15.6-45.6-42-28 52-.5 16.2-45.4 16.4 45.3h52l-41.8 28.5 16 45.4z'
-            )
-          end
+          s.path(fill: '#fff', d: 'M0 0h640v480H0z')
+          s.path(fill: '#db0000', d: 'M323 0h358v225.3H323z')
+          s.path(fill: '#0000ab', d: 'M0 225.3h323.1V480H0z')
+          s.path(
+            fill: '#0000ab',
+            fill_rule: 'evenodd',
+            d:
+              'm214.8 177.7-42-29.4-41.7 29.6 15.5-48L105 100l51.6-.4 16-48 16.3 47.9h51.6l-41.5 30 15.9 48z'
+          )
+          s.path(
+            fill: '#d80000',
+            fill_rule: 'evenodd',
+            d:
+              'm516.9 413.9-42.4-27.7-42.1 28 15.6-45.6-42-28 52-.5 16.2-45.4 16.4 45.3h52l-41.8 28.5 16 45.4z'
+          )
         end
       end
     end
